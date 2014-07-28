a71be942a6a6668263dc83fac3c75d0d40933464
Jlit1001 <jordanlittell@yahoo.com>
2014-07-28 14:00:54 -0700

    updated photo gallery front-end

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,7 +11,21 @@ class PhotosController < ApplicationController
 	def index
 		@photos = Photo.all();
 	end
-
+	def edit
+		@photo = Photo.find(params[:id])
+	end
+	def update
+		@photo = Photo.find(params[:id])
+		if @photo.save
+			redirect_to photos_path, :notice => "update successful"
+		end
+	end
+	def destroy 
+		@photo = Photo.find(params[:id])
+		if @photo.delete
+			redirect_to photos_path, :notice => "photo deleted"
+		end
+	end
 	def new 
 		@photo = Photo.new()
 	end
