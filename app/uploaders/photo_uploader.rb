4f94051977b639e9198515298dd440fe359f632a
Jlit1001 <jordanlittell@yahoo.com>
2014-07-30 20:24:43 -0700

    optimally resize images before saving

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,6 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
 
   # Choose what kind of storage to use for this uploader:
@@ -25,16 +26,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [200, 250]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :big do
+    process :resize_to_fit => [400, 500]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
