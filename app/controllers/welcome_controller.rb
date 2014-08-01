6d055b5d215c91234883445bb66ea8dc11c2d66d
Jlit1001 <jordanlittell@yahoo.com>
2014-08-01 15:18:13 -0700

    changed some files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@articles = Article.order(date: :desc);
-  	@photos = Photo.order(created_at: :desc);
+  	@articles = Article.order(date: :asc);
+  	@photos = Photo.order(created_at: :asc);
   end
 end
